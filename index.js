51f29584257c482a4903db8157e270b5579cb5d3
DESKTOP-PVQB0H3\abhia <abhistreamslive@gmail.com>
2021-10-17 19:13:16 +0530

    Epic

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("0.2+0.1")
+
+document.getElementById("RickRoll").onclick = function() {
+console.log("Get Rick Rolled idiot")
+window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")	
+	
+}
